3af851cdd3abc61739d6c9af045515b3b2475dda
rixquli <81580332+rixquli@users.noreply.github.com>
2025-03-24 10:25:11 +0100

    Add files via upload

--- db.py
+++ db.py
@@ -78,12 +78,20 @@ def get_historique(code_barre,password):
         })
     return format_list
 
-def get_produits():
-    res = cur.execute(
-        """
-        SELECT *
-            FROM Produit JOIN Stock ON Produit.id = Stock.id
-        """)
+def get_produits(exclu_null = True):
+    if exclu_null:
+        res = cur.execute(
+            """
+            SELECT *
+                FROM Produit
+            WHERE quantite > 0
+            """)
+    else:
+        res = cur.execute(
+            """
+            SELECT *
+                FROM Produit
+            """)
     res = res.fetchall()
     if res == None:
         return None
@@ -96,7 +104,7 @@ def get_produits():
             "classe": ligne[3],
             "durée": ligne[4],
             "image": ligne[5],
-            "quantite": ligne[7],
+            "quantite": ligne[6],
         })
     return format_list
 
@@ -125,16 +133,50 @@ def get_reservation_client(username):
             "classe": ligne[8],
             "image": ligne[10],
             "quantite": ligne[3],
-            "date": ligne[4]
+            "date": ligne[4],
+            "id_reservation": ligne[0]
         })
     return format_list
 
-def get_classe():
+def get_reservations():
     res = cur.execute(
         """
-        SELECT DISTINCT Classe.id, Classe.nom, Classe.couleur 
-            FROM Classe JOIN produit ON Classe.id = produit.classe
+        SELECT *
+            FROM Reservation JOIN Produit ON Reservation.produit = Produit.id JOIN Client ON Client.code_barre = Reservation.client
         """)
+    res = res.fetchall()
+    if res == None:
+        return None
+    format_list=[]
+    for ligne in res:  
+        format_list.append({
+            "id_produit": ligne[2],
+            "nom": ligne[6],
+            "prix": ligne[7],
+            "classe": ligne[8],
+            "image": ligne[10],
+            "quantite": ligne[3],
+            "date": ligne[4],
+            "id_reservation": ligne[0],
+            "nom_client": ligne[14],
+            "prenom": ligne[15]
+        })
+    return format_list
+    
+
+def get_classe(showEmpty = False):
+    if showEmpty:
+        res = cur.execute(
+            """
+            SELECT DISTINCT Classe.id, Classe.nom, Classe.couleur 
+                FROM Classe 
+            """)
+    else:
+        res = cur.execute(
+            """
+            SELECT DISTINCT Classe.id, Classe.nom, Classe.couleur 
+                FROM Classe JOIN produit ON Classe.id = produit.classe
+            """)
     res = res.fetchall()
     if res == None:
         return None
@@ -146,6 +188,7 @@ def get_classe():
             "couleur": ligne[2],
         })
     return format_list
+
 
 """
 liste_produits = {
@@ -167,7 +210,7 @@ def paiement(liste_produits, code_barre, password):
         return False
     
     for prod in liste_produits:
-        quantite_stock = int(cur.execute("Select quantite From Stock Where id = ?", (prod.id_produit)).fetchone()[0])
+        quantite_stock = int(cur.execute("Select quantite From Produit Where id = ?", (prod.id_produit)).fetchone()[0])
         if prod.quantite > quantite_stock:
             return False
         
@@ -204,13 +247,13 @@ def ajouter_sous(compte_id, montant_ajoute):
 # Fonction pour gérer le stock dans la base de données
 def gerer_stock(produit_id, quantite=0):
     # Vérification de l'existence du produit
-    cur.execute("SELECT quantite FROM stock WHERE id = ?", (produit_id,))
+    cur.execute("SELECT quantite FROM Produit WHERE id = ?", (produit_id,))
     produit = cur.fetchone()
     
     if produit:
         # Ajouter de la quantité au stock
         nouvelle_quantite = quantite
-        cur.execute("UPDATE stock SET quantite = ? WHERE id = ?", (nouvelle_quantite, produit_id))
+        cur.execute("UPDATE Produit SET quantite = ? WHERE id = ?", (nouvelle_quantite, produit_id))
         print(f"{quantite} unités ont été ajoutées. Nouveau stock : {nouvelle_quantite} unités.")
     else:
         print("Produit non trouvé.")
@@ -273,8 +316,82 @@ def ajouter_panier(code_barre, id_produit, quantite):
     if(amount[0] <= 0):
         cur.execute("DELETE FROM ProduitDansPanier WHERE id_produit = ? AND id_client  = ? ", (id_produit,code_barre,))
         con.commit()
+        
+def assez_de_produit(id_produit, quantite):
+    produits = cur.execute(
+        """
+        SELECT quantite
+            FROM Produit WHERE id  = ? 
+        """, (id_produit,))
+    produit = produits.fetchone()
+    total = produit[0]
+    return total + quantite >= 0
+        
+def modifier_produit_quantite(id_produit, quantite):
+    if assez_de_produit(id_produit, quantite):    
+        cur.execute("""
+                UPDATE Produit
+                SET quantite = quantite + ?
+                WHERE id = ? 
+            """, (quantite, id_produit))
+        con.commit()
+        return True
+    return False
+
     
+def valider_panier(code_barre, date):
+    produits = cur.execute(
+        """
+        SELECT id_produit, quantite
+            FROM ProduitDansPanier WHERE id_client  = ? 
+        """, (code_barre,))
+    produits = produits.fetchall()
+    total = get_panier_total(code_barre)
+    account = get_account(code_barre)
     
+    if account["solde"] >= total:   
+        for produit in produits:
+            if not assez_de_produit(produit[0], produit[1]):
+                return False
+        cur.execute("""
+            UPDATE Client
+            SET solde = solde - ?
+            WHERE code_barre = ? 
+        """, (total, code_barre))
+        for produit in produits:
+            for i in range(produit[1]):
+                cur.execute("""
+                                INSERT INTO Reservation (client, produit,quantite, date) VALUES
+                                (?,?,1,?)
+                            """, (code_barre, produit[0], date))
+            con.commit()
+        for produit in produits:
+            modifier_produit_quantite(produit[0],-produit[1])
+            cur.execute("DELETE FROM ProduitDansPanier WHERE id_produit = ? AND id_client  = ? ", (produit[0],code_barre,))
+        con.commit()
+        return True
+    return False
+
+def retirer_reservation(id_resevation, code_barre):
+    id_produit = cur.execute(
+        """
+        SELECT produit
+            FROM Reservation WHERE id = ?
+        """, (id_resevation,)).fetchone()[0]
+    prix_produit = cur.execute(
+        """
+        SELECT prix
+            FROM Produit WHERE id = ?
+        """, (id_produit,)).fetchone()[0]
+    cur.execute("""
+            UPDATE Client
+            SET solde = solde + ?
+            WHERE code_barre = ? 
+        """, (prix_produit, code_barre))
+    modifier_produit_quantite(id_produit,1)
+    cur.execute("DELETE FROM Reservation WHERE id = ?", (id_resevation,))
+    con.commit()
+
 #temp()
 classe ={
     "Chaud": 1,
@@ -283,9 +400,27 @@ classe ={
 }
 def ajouter_produit(nom, prix, classe, image="",quantite = 0):
     cur.execute("""
-            INSERT INTO Produit (nom, prix, classe,image, quantite) VALUES
-                        (?,?,?,?,?)
-        """, (nom, prix, classe,image, quantite))
+            INSERT INTO Produit (nom, prix, classe, quantite) VALUES
+                        (?,?,?,?)
+        """, (nom, prix, classe, quantite))
+    con.commit()
+    return cur.lastrowid
+    
+def set_produit_image(id, extension):
+    cur.execute("""
+            UPDATE Produit SET image = ? WHERE id = ?
+        """, (f"/static/produits/{id}.{extension}", id))
+    con.commit()
+
+def supprimer_produit(id_produit):
+    cur.execute("DELETE FROM Produit WHERE id = ?", (id_produit,))
+    con.commit()
+    
+def modfifier_produit(id, nom, prix, classe, image):
+    if image == None:
+        cur.execute("Update Produit Set nom = ?, prix = ?, classe = ? Where id = ?", ( nom, prix, classe, id))
+    else:    
+        cur.execute("Update Produit Set nom = ?, prix = ?, classe = ?, image = ? Where id = ?", ( nom, prix, classe, image, id))
     con.commit()
     
 def generate_password(length=12):
